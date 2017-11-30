ebd40a617410fbb8278f45956350f5de6ecd4e3e
Lara <lara.chechh@gmail.com>
2017-11-30 20:01:41 +0200

    endu15

--- mysite/views.py
+++ mysite/views.py
@@ -12,5 +12,5 @@ def home(request):
 
 
 def pizza_detail(request, pizza_id):
-    pizza = get_object_or_404(Pizza, id='pizza_id')
+    pizza = get_object_or_404(Pizza, id=pizza_id)
     return render(request, 'pizza_detail.html', {'apizza': pizza})
--- uddjango/urls.py
+++ uddjango/urls.py
@@ -24,7 +24,8 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home, name='index'),
+    url(r'^$', views.home, name='home1'),
+    url(r'^(?P<pizza_id>\d+)/$', views.pizza_detail, name='pizza-detail'),
     # url(r'^$', views.home, name='home'),
     # url(r'^test_app/', include('testurlapp.test_urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
